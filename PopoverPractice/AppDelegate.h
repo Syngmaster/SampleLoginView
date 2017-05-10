b10b24e2a6d17876dc550c76dc94fe00b8d2cff1
Syngmaster <max6361@mail.ru>
2017-05-10 14:27:32 +0100

    Initial Commit

---
+++ PopoverPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopoverPractice
+//
+//  Created by Syngmaster on 10/05/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PopoverPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopoverPractice
+//
+//  Created by Syngmaster on 10/05/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
